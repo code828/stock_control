1864866c573adb262e4964886480ef21d1910870
qianwei1970 <1909800617@qq.com>
2019-12-04 16:07:01 +0800

    20191204

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,30 @@
 <template>
   <div>
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column prop="code" label="资产代码" width="180"></el-table-column>
+      <el-table-column prop="name" label="资产名称" width="180"></el-table-column>
+      <el-table-column prop="bar_tmp" label="临时条码"></el-table-column>
+      <el-table-column prop="barcode" label="总行条码"></el-table-column>
+      <el-table-column prop="place" label="放置地点"></el-table-column>
+      <el-table-column prop="detail" label="备注"></el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
+//import { queryStocks } from '../api/api'
+import axios from 'axios'
 export default {
   data() {
     return {
-      tableData: []
-    };
+      tableData: [],
+    }
   },
-  mounted() {
-    this.axios.get("http://127.0.0.1:8000/backend/stocks/").then(body =>{
-      this.tableData = body.data;
+  created() {
+    var that = this
+    axios.get('http://127.0.0.1:8000/backend/stocks/').then(function (res) {
+      console.log(res);
+      that.tableData = res.data;
     })
   }
 };
